7b954554e4d12a0d4edbaafcc5379c81c5ccb2df
Joao Meyer <1994meyer@gmail.com>
2026-01-20 21:15:56 -0300

    Fixed SSR

--- app/plugins/trpc.ts
+++ app/plugins/trpc.ts
@@ -1,13 +1,10 @@
 import {
-  httpBatchLink,
-  httpLink,
   isNonJsonSerializable,
   loggerLink,
   splitLink,
   type TRPCLink,
 } from '@trpc/client'
-import { observable } from '@trpc/server/observable'
-import { createTRPCNuxtClient } from 'trpc-nuxt/client'
+import { createTRPCNuxtClient, httpBatchLink, httpLink } from 'trpc-nuxt/client'
 import type { AppRouter } from '~~/server/trpc'
 
 /**
@@ -27,49 +24,21 @@ import type { AppRouter } from '~~/server/trpc'
  * transparency.
  */
 export default defineNuxtPlugin(async () => {
+  const url = useRequestURL()
+  url.pathname = '/api/trpc'
+
   const links: TRPCLink<AppRouter>[] = []
 
-  if (import.meta.server) {
-    const event = useRequestEvent()
-    if (!event) throw new Error('No event found')
+  // Client-side initialization using standard HTTP transport. We use
+  // splitLink to handle both standard JSON and non-serializable data (like
+  // Files).
+  if (import.meta.client) links.push(loggerLink())
 
-    const { appRouter } = await import('~~/server/trpc')
-    const { createInternalCaller } = await import('~~/server/trpc/init')
-
-    const caller = await createInternalCaller(appRouter, event!)
-
-    // Internal link to bypass HTTP during SSR. This transforms tRPC calls into
-    // direct function calls against the router, ensuring we stay within the
-    // same execution context as the Nitro event
-    links.push(() => ({ op }) => observable((observer) => {
-      const parts = op.path.split('.')
-
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      let procedure = caller as any
-      for (const part of parts) procedure = procedure[part]
-
-      procedure(op.input)
-        .then((data: unknown) => {
-          observer.next({ result: { data } })
-          observer.complete()
-        })
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        .catch((err: any) => observer.error(err))
-    }))
-  }
-
-  if (import.meta.browser) {
-    // Client-side initialization using standard HTTP transport. We use
-    // splitLink to handle both standard JSON and non-serializable data (like
-    // Files).
-    links.push(loggerLink())
-
-    links.push(splitLink({
-      condition: op => isNonJsonSerializable(op.input),
-      true: httpLink({ url: '/api/trpc' }),
-      false: httpBatchLink({ url: '/api/trpc' }),
-    }))
-  }
+  links.push(splitLink({
+    condition: op => isNonJsonSerializable(op.input),
+    true: httpLink({ url }),
+    false: httpBatchLink({ url }),
+  }))
 
   const trpc = createTRPCNuxtClient({ links })
   return { provide: { trpc } }
